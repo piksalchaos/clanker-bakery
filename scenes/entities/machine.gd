0e2e94191264c2507b5a645150e41873cbe23fe9
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2026-02-18 17:14:17 -0800

    new sprites for machines
    will def change the icing machines

--- scenes/entities/machine.gd
+++ scenes/entities/machine.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 @export var ingredient: IngredientTypes.Ingredient
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+
+func _ready() -> void:
+	sprite.animation = str(ingredient)
 
 
 func _on_area_entered(cake: Cake) -> void:
 	cake.add_ingredient(ingredient)
+	sprite.play()
